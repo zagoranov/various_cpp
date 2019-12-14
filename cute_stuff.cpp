87e93a812a81779e938b75cfa897fef71b687ee1
zagoranov <pijammer@gmail.com>
2019-12-14 21:27:58 +0300

    Apply lambda to all members of tuple<>

--- cute_stuff.cpp
+++ cute_stuff.cpp
@@ -1,4 +1,5 @@
 
+//--------------------------------------------------------------------------------
 // all_of()
 
 template<class... Args, class Predicate>
@@ -8,11 +9,32 @@ void AllOf(Args&&... args, Predicate predicate) {
 
 }
 
+//--------------------------------------------------------------------------------
 // cozy initializer
 
 template<class... Args>
 void Log(Args&&... args) {
 	(std:cerr << ... << args) << '\n';
 }
+//--------------------------------------------------------------------------------
+// Apply lambda to all members of tuple<> 
+// auto f = [](int x, double y, double z) { return x + y + z; };
+// auto t = std::make_tuple(30, 5.0, 1.6);  // std::tuple<int, double, double>
+// auto res = apply(f, t); 
 
+//Better new realzation
+template<typename... Args>
+void apply2(std::function<double(int, double, double)> N, Args&... args) {
+    N(std::forward<Args>(args)...);
+}
 
+//Old poorer realization
+template<std::size_t I = 0, typename F, typename... Tp, typename... Tp2>
+auto apply(F func, std::tuple<> t, Tp2... args) -> decltype(F(args...))
+{
+    return func(args...);	
+}
+template<std::size_t I = 0, typename F, typename... Tp, typename... Tp2>
+auto apply(F func, std::tuple<Tp...> t, Tp2... args) -> decltype(F(args...)) {
+	apply<I + 1, F, Tp..., Tp2...>(func, t, std::get<I>(t), args...);
+}
