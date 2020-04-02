d70ddc0fe980967e700638deac902c9751cca339
zagoranov <pijammer@gmail.com>
2020-04-02 21:09:08 +0300

    new stuff

--- variadic_inheritance.cpp
+++ variadic_inheritance.cpp
@@ -1,13 +1,41 @@
+#include <string>
+
+
 template<typename... Mixins>
 class Point : public Mixins...{
     public:
     Point() : Mixins()... {}
 };
 
-struct Color {};
-struct Label {};
+template<typename... Mixins>
+class PointVisit : public Mixins...{
+    public:
+    PointVisit() : Mixins()... {}
+    
+    template<typename Visitor>
+    void visitMixins(Visitor visitor)
+    {
+        visitor(static_cast<Mixins&>(*this)...);
+    }
+};
+
+template<typename... Mixins>
+class PointCtor : public Mixins...{
+    public:
+    PointCtor(Mixins... mixins) : Mixins(mixins)... {}
+};
+
+
+struct Color {
+    char red, green, blue;
+};
+struct Label {
+    std::string name;
+};
+
 
 int main() {
     Point<Label, Color> p;
     
+    PointCtor<Color, Label> p2({0x7f, 0, 0x7f}, {"center"});
 }
