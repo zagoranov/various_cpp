97a45e8bbfc6c0d2c8e6e722a1eca28edf3e9e26
zagoranov <pijammer@gmail.com>
2020-07-09 22:02:45 +0300

    type and value traits

--- template_typevalue_traits.cpp
+++ template_typevalue_traits.cpp
@@ -6,19 +6,21 @@ struct AccTraits;
 template<>
 struct AccTraits<char>{
     using AccT = int;
+    static AccT const zero = 0;
 };
 
 
 template<>
 struct AccTraits<int>{
     using AccT = long;
+    static AccT const zero = 0;
 };
 
 
 template<typename T>
 auto accum(T const* beg, T const* end) {
     using AccType = typename AccTraits<T>::AccT;
-    AccType total{};
+    AccType total = AccTraits<T>::zero;
     while(beg != end) {
         total += *beg;
         ++beg;
@@ -31,7 +33,7 @@ int main() {
     int iarr[] = {1,2,3,4,5};
     std::cout << accum(iarr, iarr + 5) << std::endl;
     
-    char carr[] = "123456";
-    std::cout << accum(carr, carr);
+    char carr[] = "12345";
+    std::cout << accum(carr, carr + 5);
     
 }
