8ff2f9354aaad9e892eb9f660ac2eddf019bd694
zagoranov <pijammer@gmail.com>
2020-07-09 23:48:30 +0300

    Update template_typevalue_traits.cpp

--- template_typevalue_traits.cpp
+++ template_typevalue_traits.cpp
@@ -23,15 +23,6 @@ struct AccTraits<float>{
 };
 
 
-/*class SumPolicy {
-public:
-    template<typename T1, typename T2> 
-    static void accumulate(T1& total, T2 const& value) {
-        total += value;
-    }
-
-};*/
-
 template<typename T1, typename T2> 
 class SumPolicy {
 public:
