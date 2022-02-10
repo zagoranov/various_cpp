b5b48adb732b5f731b8584b89f43d2e978f9056e
Roman Muzyukin <pijammer@gmail.com>
2022-02-10 20:29:49 +0300

    little update

--- functional_cpp_book.cpp
+++ functional_cpp_book.cpp
@@ -31,6 +31,8 @@ private:
 //error_test_t not_error(false);
 
 
+
+
 // 4.1 Partial function application
 
 template <typename Function, typename SecondArgType>
@@ -57,3 +59,18 @@ private:
     SecondArgType m_second_arg;
 };
 
+
+template <typename Function, typename SecondArgType>
+partial_application_bind2nd_impl<Function, SecondArgType>
+bind2nd(Function&& function, SecondArgType&& second_arg)
+{
+    return partial_application_bind2nd_impl<Function, SecondArgType>(
+            std::forward<Function>(function),
+            std::forward<SecondArgType>(second_arg));
+}
+
+
+//auto greater_than_42 = bind2nd(std::greater<int>(), 42);
+//greater_than_42(1);  / false
+//greater_than_42(50); / true
+//std::partition(xs.begin(), xs.end(), bind2nd(std::greater<int>(), 6));
